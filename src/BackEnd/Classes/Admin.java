b36ddde7bb591b4813659f140a3ac6ec4222ae14
oguzcannn <117283308+oguzcannn@users.noreply.github.com>
2024-12-03 22:08:36 +0300

    Admin sınıfında küçük bir değişiklik yapıldı.

--- src/BackEnd/Classes/Admin.java
+++ src/BackEnd/Classes/Admin.java
@@ -4,5 +4,5 @@ public class Admin extends User{
     public Admin(String user_id, String[] friends_id, String user_name, String user_password, String admin_level) {
         super(user_id, friends_id, user_name, user_password);
     }
-    }
 }
+
