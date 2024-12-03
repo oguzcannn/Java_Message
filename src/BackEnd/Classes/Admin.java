aa86b7c81037bf34dc29a146ba8e6ae0fe4cc735
oguzcannn <117283308+oguzcannn@users.noreply.github.com>
2024-12-03 22:06:07 +0300

    User, admin ve mesaj sınıflarının temeli atıldı.

---
+++ src/BackEnd/Classes/Admin.java
@@ -0,0 +1,8 @@
+package BackEnd.Classes;
+
+public class Admin extends User{
+    public Admin(String user_id, String[] friends_id, String user_name, String user_password, String admin_level) {
+        super(user_id, friends_id, user_name, user_password);
+    }
+    }
+}
